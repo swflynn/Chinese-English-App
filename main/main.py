df9e06782f7ad397cdad583aca6143c8585688a7
swflynn <swflynn@uci.edu>
2020-03-03 20:13:55 -0800

    Initial Main

---
+++ main/main.py
@@ -0,0 +1,14 @@
+#=================20==================40==================60==================80
+#                   Chinese-English Flashcard App.
+#==============================================================================!
+#Generate gridpoints distributed via the 2D Morse Oscillator
+#This Quasi-Regular gird is optimized using a quasi-Lennard Jones Potential
+#Force minimization, accept any trial moves that reduces the system's energy
+#See MMC grid for metropolis optimization (acceptance criteria)
+#==============================================================================!
+#       Modified:
+#   3 March 2020
+#       Authors:
+#   Chris Zhang & Shane Flynn
+#==============================================================================!
+print("Hello Universe!")
